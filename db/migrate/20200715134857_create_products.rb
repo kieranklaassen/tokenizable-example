6dfbd110ecdd3bb1362b7f3f987da24e97ba1fda
Kieran Klaassen <kieranklaassen@gmail.com>
2020-07-15 08:52:41 -0500

    Initial commit

---
+++ db/migrate/20200715134857_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :description
+      t.decimal :price
+      t.string :token
+
+      t.timestamps
+    end
+    add_index :products, :token
+  end
+end
